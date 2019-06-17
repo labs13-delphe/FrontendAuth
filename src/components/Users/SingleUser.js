322be7299e1b7ad872b134825cc41040d3b492c7
AmarachiOE <ao.ejiawoko@gmail.com>
2019-06-17 13:54:55 -0700

    Added 'hourly rate' on profile

--- src/components/Users/SingleUser.js
+++ src/components/Users/SingleUser.js
@@ -5,11 +5,11 @@ const SingleUser = props => {
 
   return (
     <>
-    <h3>{props.user.first_name} {props.user.last_name}, {props.user.user_type}</h3>
-    <h4>username: {props.user.username}</h4>
-    <h4>bio: {props.user.bio}</h4>
-    <h4>email: {props.user.email}</h4>
-    <h4>{props.user.hourly_rate}</h4>
+    <h3>{props.user.first_name} {props.user.last_name}</h3>
+    <h4>@{props.user.username}, {props.user.user_type}</h4>
+    <h4>Bio: {props.user.bio}</h4>
+    <h4>Email: {props.user.email}</h4>
+    <h4>Hourly Rate: {props.user.hourly_rate}</h4>
     </>
   );
 }
