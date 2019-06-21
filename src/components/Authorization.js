4dddcb92d182fef2135f05900bd4473fa4a586bd
AmarachiOE <ao.ejiawoko@gmail.com>
2019-06-21 11:00:53 -0700

    Styling authorization page

--- src/components/Authorization.js
+++ src/components/Authorization.js
@@ -3,7 +3,29 @@ import React, { Component } from "react";
 import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
+// Materiall UI
+import {
+  withStyles,
+  CssBaseline,
+  Typography,
+  Paper,
+  Grid
+} from "@material-ui/core";
+
 // NOTE: isSignedIn and gUser on App.js state is also being set through this file
+
+const styles = theme => ({
+  center: {
+    display: "flex",
+    flexDirection: "column",
+    //alignItems: "center",
+    position: 'absolute', 
+        left: '50%', 
+        top: '50%',
+        transform: 'translate(-50%, -50%)',
+    padding: theme.spacing(3)
+  }
+});
 
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
@@ -43,18 +65,26 @@ class Authorization extends Component {
   };
 
   render() {
+    const { classes } = this.props;
     console.log("Authorization state", this.state);
 
     return (
       <div>
-        <h1>Please register</h1>
-            <StyledFirebaseAuth
-              uiConfig={this.uiConfig}
-              firebaseAuth={firebase.auth()}
-            />
+        <CssBaseline />
+        <Paper className={classes.center}>
+<Typography variant="h4">Let's Create an Account</Typography>
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+</Paper>
+
+        {/* <Grid container justify="center">
+          
+        </Grid> */}
       </div>
     );
   }
 }
 
-export default Authorization;
+export default withStyles(styles)(Authorization);
