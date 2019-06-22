610be5ee54ff22073e24badc6cecffa3fdf3be59
Josh Akeman <joshakeman@gmail.com>
2019-06-22 10:38:37 -0400

    cleaned up code in expert dashboard, removed unecessary commented out sections

--- src/components/ExpertDashboard/Question.js
+++ src/components/ExpertDashboard/Question.js
@@ -282,9 +282,7 @@ class Question extends React.Component {
   render() {
     const { answers, users, thisUser, answerCount, question, topics, dialogOpen, expanded, askerInfo } = this.state,
           { classes } = this.props
-    // console.log("question props", this.props);
-    // console.log("question state", this.state);
-    // condition: Render Answers Div if question has answers (answerCount > 0)
+  
     const answersDiv =
       this.state.answerCount > 0 ? (
         <div className="answers-div">
@@ -327,30 +325,6 @@ class Question extends React.Component {
                       <DialogContent className="form">
                         <Typography> Title: {question.title}</Typography>
                         <Typography> Question: {question.question}</Typography>
-
-                        {/* <TextField
-                          value={question.title}
-                          name="title"
-                          label="Title"
-                          placeholder="Question title..."
-                          multiline
-                          className={classes.textField}
-                          onChange={this.handleChanges}
-                          margin="normal"
-                          variant="outlined"
-                        />
-                        <TextField
-                          value={question.question}
-                          name="question"
-                          label="Question"
-                          placeholder="I want to know..."
-                          multiline
-                          className={classes.textField}
-                          onChange={this.handleChanges}
-                          margin="normal"
-                          variant="outlined"
-                        /> */}
-
                       <TextField
                           value={this.state.singleAnswer.answer}
                           name="answer"
@@ -384,20 +358,6 @@ class Question extends React.Component {
                       
                       </DialogActions>
                     </Dialog>
-                    {/* <button
-                      onClick={e => {
-                        this.handleEdit(e, answer.id);
-                      }}
-                    >
-                      Edit
-                    </button>
-                    <button
-                      onClick={e => {
-                        this.deleteAnswer(e, answer.id);
-                      }}
-                    >
-                      Delete
-                    </button> */}
                   </ListItem>
                 </div>
               );
@@ -407,45 +367,7 @@ class Question extends React.Component {
       ) : (
         <p>No answers yet</p>
       );
-          {/* <p style={generalAlign}>
-            <strong>Answers: </strong>
-          </p>
-          {this.state.answers.map(answer => {
-            return (
-              <p style={answerStyle} key={answer.id}>
-                <button
-                  onClick={e => {
-                    this.handleEdit(e, answer.id);
-                  }}
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={e => {
-                    this.deleteAnswer(e, answer.id);
-                  }}
-                >
-                  Delete
-                </button>
-                "{answer.answer}" -{" "}
-                <strong style={expertName}>
-                  {this.state.users.map(user => {
-                    if (user.id === answer.user_id) {
-                      return (
-                        <Link to={`/users/${user.id}`} key={user.id}>{user.username}</Link>
-                      );
-                    } else {
-                      return null;
-                    }
-                  })}
-                </strong>
-              </p>
-            );
-          })}
-        </div>
-      ) : (
-        <p>No answers yet</p>
-      ); */}
+          
       const answersText = this.state.answerCount === 1 ? <span>answer</span> : <span>answers</span>;
     return (
       <>
@@ -461,77 +383,7 @@ class Question extends React.Component {
           action={
             <>
             
-              {/* <IconButton aria-label="Settings" onClick={this.handleClickOpen}>
-               <Edit  />
-              </IconButton>
-               <IconButton aria-label="Settings" onClick={this.deleteButton}>
-                 <Delete />
-               </IconButton>
-               
-               <Dialog
-                open={dialogOpen}
-                 onClose={this.handleClose}
-                 className={classes.dialog}
-                 aria-labelledby="form-dialog-title"
-               >
-                 <DialogTitle id="form-dialog-title">
-                   Edit Your Question
-                 </DialogTitle>
-                 <DialogContent className="form">
-                   <TextField
-                     value={question.title}
-                     name="title"
-                     label="Title"
-                     placeholder="Question title..."
-                     multiline
-                     className={classes.textField}
-                     onChange={this.handleChanges}
-                     margin="normal"
-                     variant="outlined"
-                   />
-                   <TextField
-                    value={question.question}
-                     name="question"
-                    label="Question"
-                     placeholder="I want to know..."
-                     multiline
-                     className={classes.textField}
-                     onChange={this.handleChanges}
-                     margin="normal"
-                     variant="outlined"
-                   />
-                 </DialogContent>
-                 <DialogActions className="formButtons">
-                   <Button
-                    onClick={this.handleClose}
-                     variant="contained"
-                     color="primary"
-                     className={classes.button}
-                   >
-                     Cancel
-                   </Button>
-                   {question.title && question.question ? (
-                     <Button
-                       variant="contained"
-                       color="primary"
-                       className={classes.button}
-                       onClick={this.submitForm}
-                    >
-                      Submit
-                     </Button>
-                 ) : (
-                     <Button
-                       variant="contained"
-                       color="primary"
-                       disabled
-                       className={classes.button}
-                     >
-                       Submit
-                     </Button>
-                   )}
-                 </DialogActions>
-               </Dialog>  */}
-            </>
+             </>
           }
           title={askerInfo.username}
           subheader={askerInfo.user_type}
@@ -600,149 +452,9 @@ class Question extends React.Component {
           <Typography paragraph>{answersDiv}</Typography>
         </Collapse>
       </Card>
-        {/* <div >
-        <div className="question-div">
         
-            {this.state.users.map(user => {
-              if (user.id === this.state.question.user_id) {
-                return (
-                  <div className="user-info-div"  key={user.id}>
-                    <p>
-                      {user.first_name} {user.last_name} @{user.username}
-                      <Link to={`/users/${user.id}`}>View Profile</Link>
-                    </p>
-                  </div>
-                );
-              } else {
-                return null;
-              }
-            })}
-          
-          <p >
-            {/* <Link to={`/questions/${this.state.question.id}/update`}>
-              <i className="fas fa-pen" />
-            </Link>
-            <i onClick={this.deleteButton} className="fas fa-trash" />
-            &nbsp;|&nbsp; */}
-            {/* <strong>{this.state.question.title}: </strong>
-            {this.state.question.question} <br /> {this.state.answerCount}{" "}
-            {answersText}
-          </p>
-        </div>
-
-        <div className="topics-div">
-          <p >
-            <strong>Topic: </strong>
-            {this.state.topics.map(topic => (
-              <span key={topic.id}>{topic.topic}, </span>
-            ))}
-          </p>
-        </div>
-        {answersDiv}
-
-        <div>
-          {this.state.isEditing ? (
-            <form onSubmit={this.submitEdit}>
-              <input
-                label="singleAnswer"
-                type="text"
-                name="answer"
-                value={this.state.singleAnswer.answer}
-                placeholder="answer"
-                onChange={this.handleEditChange}
-                className="answer-input"
-              />
-              <button onClick={this.submitEdit}>Save Edit</button>
-            </form>
-          ) : (
-            <form onSubmit={this.submitAnswer}>
-              <input
-                label="answer"
-                type="text"
-                name="answer"
-                value={this.state.answer}
-                placeholder="answer"
-                onChange={this.handleChange}
-                className="answer-input"
-              />
-              <button onClick={this.submitAnswer}>Submit</button>
-            </form>
-          )}
-        </div>
-      </div> */}
       </>
     );
-    //   <div style={bordered}>
-    //     <div className="question-div">
-        
-    //         {this.state.users.map(user => {
-    //           if (user.id === this.state.question.user_id) {
-    //             return (
-    //               <div className="user-info-div" style={generalAlign} key={user.id}>
-    //                 <p>
-    //                   {user.first_name} {user.last_name} @{user.username}
-    //                   <Link to={`/users/${user.id}`}>View Profile</Link>
-    //                 </p>
-    //               </div>
-    //             );
-    //           } else {
-    //             return null;
-    //           }
-    //         })}
-          
-    //       <p style={generalAlign}>
-    //         {/* <Link to={`/questions/${this.state.question.id}/update`}>
-    //           <i className="fas fa-pen" />
-    //         </Link>
-    //         <i onClick={this.deleteButton} className="fas fa-trash" />
-    //         &nbsp;|&nbsp; */}
-    //         <strong>{this.state.question.title}: </strong>
-    //         {this.state.question.question} <br /> {this.state.answerCount}{" "}
-    //         {answersText}
-    //       </p>
-    //     </div>
-
-    //     <div className="topics-div">
-    //       <p style={generalAlign}>
-    //         <strong>Topic: </strong>
-    //         {this.state.topics.map(topic => (
-    //           <span key={topic.id}>{topic.topic}, </span>
-    //         ))}
-    //       </p>
-    //     </div>
-    //     {answersDiv}
-
-    //     <div>
-    //       {this.state.isEditing ? (
-    //         <form onSubmit={this.submitEdit}>
-    //           <input
-    //             label="singleAnswer"
-    //             type="text"
-    //             name="answer"
-    //             value={this.state.singleAnswer.answer}
-    //             placeholder="answer"
-    //             onChange={this.handleEditChange}
-    //             className="answer-input"
-    //           />
-    //           <button onClick={this.submitEdit}>Save Edit</button>
-    //         </form>
-    //       ) : (
-    //         <form onSubmit={this.submitAnswer}>
-    //           <input
-    //             label="answer"
-    //             type="text"
-    //             name="answer"
-    //             value={this.state.answer}
-    //             placeholder="answer"
-    //             onChange={this.handleChange}
-    //             className="answer-input"
-    //           />
-    //           <button onClick={this.submitAnswer}>Submit</button>
-    //         </form>
-    //       )}
-    //     </div>
-    //   </div>
-    // );
   }
 }
 
