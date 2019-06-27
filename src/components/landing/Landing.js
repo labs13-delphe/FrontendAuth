442589be51dc4333d41723de2fb317778ef21980
chad kidd <ckiddwebdev@gmail.com>
2019-06-27 14:11:50 -0700

    got rid of nav on landing page

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -13,19 +13,6 @@ export class Landing extends Component {
     render() {
         return (
             <div>
-{/* <nav className="white" role="navigation">
-      <div className="nav-wrapper container">
-        <a id="logo-container" href="#" className="brand-logo">Logo</a>
-        <ul className="right hide-on-med-and-down">
-          <li><a href="#">Navbar Link</a></li>
-        </ul>
-  
-        <ul id="nav-mobile" className="sidenav">
-          <li><a href="#">Navbar Link</a></li>
-        </ul>
-        <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-      </div>
-    </nav> */}
 {/*   
     <div className="headerContainer">
       <div className="section no-pad-bot">
